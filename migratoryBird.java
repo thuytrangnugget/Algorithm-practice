07b4d1351abb708b61e0993c48961c45d77cd22f
Duơng Trang <thienthienchipcuata@gmail.com>
2020-11-11 21:21:10 +0700

    edit lai nhung van chua chay duoc

--- migratoryBird.java
+++ migratoryBird.java
@@ -1,8 +1,10 @@
+
 int migratoryBirds(int[] list_of_birds) {
     Map<Integer, Integer> mp = new HashMap<>();
     int n = list_of_birds.length;
     int[] frequency = new int[n];
-    int indexMax = 0;
+    int max = 0;
+
     for (int i = 0; i < n; i++) {
         if (mp.containsKey(list_of_birds[i])) {
             mp.put(list_of_birds[i], mp.get(list_of_birds[i]) + 1);
@@ -12,14 +14,27 @@ int migratoryBirds(int[] list_of_birds) {
         }
         frequency[i] = mp.get(list_of_birds[i]);
     }
+
     for (int j = 0; j < n - 1; j++) {
         if (frequency[j] < frequency[j + 1]) {
-            indexMax = j + 1;
-        }
-        if (frequency[indexMax] = frequency[i]) {
-            if
+            max = frequency[j + 1];
         }
     }
-    return list_of_birds[indexMax];
 
+    int[] suitableBirds;
+    for (int j = 0; j < n ; j++) {   
+        if (frequency[j] == max) {
+            suitableBirds[j] = list_of_birds[j];
+        }
+    }
 
+    int birdWeNeed = suitableBirds[0];
+    for (int k = 0; k < suitableBirds.length; k++) {
+        if (suitableBirds[k] < birdWeNeed) {
+            birdWeNeed = suitableBirds[k];
+        }
+    }
+
+    return birdWeNeed;
+
+}
